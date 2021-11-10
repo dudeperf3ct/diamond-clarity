7ccc6fa6e6b0a66795bcaec50620c412430d2132
dudeperf3ct <dudeperf3ct@users.noreply.github.com>
2021-11-10 22:05:38 +0530

    add cnn3d experiments

---
+++ model.py
@@ -0,0 +1,106 @@
+import copy
+import torch
+import torch.nn as nn
+
+from models import resnet, simplecnn3d
+from torchinfo import summary
+
+
+def set_parameter_requires_grad(model, feature_extracting: bool, num_ft_layers: int):
+    """
+    Freeze the weights of the model is feature_extracting=True
+    Fine tune layers >= num_ft_layers
+    Batch Normalization: https://keras.io/guides/transfer_learning/
+
+    Args:
+        model: PyTorch model
+        feature_extracting (bool): A bool to set all parameters to be trainable or not
+        num_ft_layers (int): Number of layers to freeze and unfreezing the rest
+    """
+    if feature_extracting:
+        if num_ft_layers != -1:
+            for i, module in enumerate(model.modules()):
+                if i >= num_ft_layers:
+                    if not isinstance(module, nn.BatchNorm3d):
+                        module.requires_grad_(True)
+                else:
+                    module.requires_grad_(False)
+        else:
+            for param in model.parameters():
+                param.requires_grad = False
+    # not recommended to set feature_extracting=True when use_pretrained=True
+    else:
+        for param in model.parameters():
+            param.requires_grad = True
+
+
+def _create_classifier(num_ftrs: int, embedding_size: int, num_classes: int):
+    """Add a classifier head with 2 FC layers
+
+    Args:
+        num_ftrs (int): Number of features from timm models
+        embedding_size (int): Number of features in penultimate layer
+        num_classes (int): Number of classes
+    """
+    head = nn.Sequential(
+        nn.Linear(num_ftrs, embedding_size),
+        nn.Linear(embedding_size, num_classes)
+    )
+    return head
+
+
+def build_models(
+        model_name: str,
+        num_classes: int,
+        in_channels: int,
+        embedding_size: int,
+        feature_extract: bool = True,
+        use_pretrained: bool = True,
+        num_ft_layers: int = -1,
+        bst_model_weights=None
+):
+    """
+    Build various architectures to either train from scratch, finetune or as feature extractor.
+
+    Args:
+        model_name (str) : Name of model from `timm.list_models(pretrained=use_pretrained)`
+        num_classes (int) : Number of output classes added as final layer
+        in_channels (int) : Number of input channels
+        embedding_size (int): Size of intermediate features
+        feature_extract (bool): Flag for feature extracting.
+                               False = finetune the whole model,
+                               True = only update the new added layers params
+        use_pretrained (bool): Pretraining parameter to pass to the model or if base_model_path is given use that to
+                                initialize the model weights
+        num_ft_layers (int) : Number of layers to finetune
+                             Default = -1 (do not finetune any layers)
+        bst_model_weights : Best weights obtained after training pretrained model
+                            which will be used for further finetuning.
+
+    Returns:
+        model : A pytorch model
+    """
+    supported_models = ['resnet10', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152', 'resnet200', 'simple_cnn3d']
+    model = None
+    if model_name in supported_models:
+        if model_name == 'resnet10':
+            model = resnet.resnet10(sample_size=384, sample_duration=6)
+        if model_name == 'resnet18':
+            model = resnet.resnet18(sample_size=384, sample_duration=6)
+        if model_name == 'simple_cnn3d':
+            model = simplecnn3d.Simple3dCNN()
+
+        set_parameter_requires_grad(model, feature_extract, num_ft_layers)
+        num_ftrs = model.fc.in_features
+        model.fc = _create_classifier(num_ftrs, embedding_size, num_classes)
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+    # load best model dict for further finetuning
+    if bst_model_weights is not None:
+        pretrain_model = torch.load(bst_model_weights)
+        best_model_wts = copy.deepcopy(pretrain_model.state_dict())
+        if feature_extract and num_ft_layers != -1:
+            model.load_state_dict(best_model_wts)
+    summary(model, input_size=(1, 3, 6, 384, 384))
+    return model
---
+++ models/simplecnn3d.py
@@ -0,0 +1,53 @@
+# based on : https://keras.io/examples/vision/3D_image_classification/
+import torch
+import torch.nn as nn
+import math
+
+class Simple3dCNN(nn.Module):
+    def __init__(self) -> None:
+        super(Simple3dCNN, self).__init__()
+        self.conv1 = nn.Conv3d(3, 64, kernel_size=11, stride=(1, 2, 2),
+                               padding=(3, 3, 3), bias=False)
+        self.bn1 = nn.BatchNorm3d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = self._make_conv_layer(64, 128, 7, (1, 2, 2), 3)
+        self.conv3 = self._make_conv_layer(128, 256, 5, (1, 2, 2), 3)
+        self.conv4 = self._make_conv_layer(256, 512, 5, (1, 2, 2), 3)
+        # self.conv5 = self._make_conv_layer(512, 1024, 5, (1, 2, 2), 3)
+        self.fc = nn.Linear(4608, 256)
+    
+    def _make_conv_layer(self, in_c, out_c, k, s, p):
+        conv_layer = nn.Sequential(
+        nn.Conv3d(in_c, out_c, kernel_size=k, stride=s, padding=p),
+        nn.LeakyReLU(),
+        nn.MaxPool3d(2),
+        nn.BatchNorm3d(out_c)
+        )
+        return conv_layer
+
+    def forward(self, x):
+        # # print(x.size())
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        # print(x.size())
+        x = self.conv2(x)
+        # print(x.size())
+        x = self.conv3(x)
+        # print(x.size())
+        x = self.conv4(x)
+        # print(x.size())
+        # x = self.conv5(x)
+        # print(x.size())
+        x = x.view(x.size(0), -1)
+        # print(x.size())
+        x = self.fc(x)
+        # # print(x.size())
+        return x
+
+
+# if __name__ == '__main__':
+#     d_in = torch.randn(1, 3, 6, 384, 384)
+#     m = Simple3dCNN()
+#     print(m)
+#     print(m(d_in).shape)
