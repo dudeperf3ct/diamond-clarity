66c066d93ebdfdd44702e7ef894e9dbc9cb5877d
shubham <shubham@smartcow.ai>
2021-11-15 16:59:25 +0530

    add enet

--- models/cnn2dlstm.py
+++ models/cnn2dlstm.py
@@ -8,10 +8,10 @@ class CNN2DLSTM(nn.Module):
     def __init__(self, model_name):
         super(CNN2DLSTM, self).__init__()
         if model_name == 'resnet18':
-            self.resnet = models.resnet18(pretrained=True)
-        if model_name == 'resnet50':
-            self.resnet = models.resnet50(pretrained=True)
-        self.resnet.fc = nn.Sequential(nn.Linear(self.resnet.fc.in_features, 256))
+            self.pretain = models.resnet18(pretrained=True)
+        if model_name == 'efficientnet':
+            self.pretain = models.efficientnet_b2(pretrained=True)
+        self.pretain.fc = nn.Sequential(nn.Linear(self.pretain.fc.in_features, 256))
         self.lstm = nn.LSTM(input_size=256, hidden_size=512, num_layers=3)
         self.fc1 = nn.Linear(512, 128)
         self.fc2 = nn.Linear(128, 2)
@@ -20,7 +20,7 @@ class CNN2DLSTM(nn.Module):
         hidden = None
         for t in range(x_3d.size(1)):
             with torch.no_grad():
-                x = self.resnet(x_3d[:, :, t, :, :])  
+                x = self.pretain(x_3d[:, :, t, :, :])  
             out, hidden = self.lstm(x.unsqueeze(0), hidden)         
 
         x = self.fc1(out[-1, :, :])
